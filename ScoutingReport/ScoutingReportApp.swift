c6c25575b3b3f36ed6962ea0141a50e82858fe25
ryan-montoyo <ryanmontoyo2003@gmail.com>
2024-07-09 16:07:30 -0400

    Initial Commit

---
+++ ScoutingReport/ScoutingReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutingReportApp.swift
+//  ScoutingReport
+//
+//  Created by Ryan Montoyo on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ScoutingReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
